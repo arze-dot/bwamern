28e604468787ae074dbf97fd5963fd56bc1d59b2
ezrazra <62278480+arze-dot@users.noreply.github.com>
2021-04-27 22:33:30 +0700

    finished component most-picked and categories

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -6,11 +6,11 @@ import propTypes from 'prop-types';
 
 export default function Button(props) {
     const className = [props.className]
-    if(props.isPrimary) className.push("btn-primary");
-    if(props.isLarge) className.push("btn-lg");
-    if(props.isSmall) className.push("btn-sm");
-    if(props.isBlock) className.push("btn-block");
-    if(props.hasShadow) className.push("btn-shadow");
+    if(props.isPrimary) className.push(" btn-primary ");
+    if(props.isLarge) className.push("btn-lg ");
+    if(props.isSmall) className.push("btn-sm ");
+    if(props.isBlock) className.push("btn-block ");
+    if(props.hasShadow) className.push("btn-shadow ");
 
     const onClick = () => {
         if(props.onClick) props.onClick();
@@ -77,11 +77,10 @@ Button.propTypes = {
     className: propTypes.string,
     isDisabled: propTypes.bool,
     isExternal: propTypes.bool,
+    isPrimary: propTypes.bool,
     isLoading: propTypes.bool,
     isSmall: propTypes.bool,
     isLarge: propTypes.bool,
     isBlock: propTypes.bool,
     hasShadow: propTypes.bool,
-
-
 }
